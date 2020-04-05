4224c307e65cf38a57165965745116107debb0a9
Crowdbotics <team@crowdbotics.com>
2020-04-05 12:29:47 +0000

    Update models

--- infinite_test_4_dev_2338/wsgi.py
+++ infinite_test_4_dev_2338/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infinite_test_4_dev_2338.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infinite_test_4_dev_2338.settings")
 
 application = get_wsgi_application()
